3e1802e4c050a0e1b502a9f24864bcecc3a47edb
Lukas <lukasimbm@gmail.com>
2021-03-30 07:56:08 +0300

    video 56

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,17 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/40"
+            alt=""
+            class="rounded-full mr-4"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">Jogn Doe</h5>
+
+        <p class="text-sm">
+            Presumably, if you're watching this series, you've already made the decision.
+        </p>
+    </div>
+</div>
