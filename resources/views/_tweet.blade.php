cc83524c67bf3198266c519a69d90aa1ffaadbf6
Lukas <lukasimbm@gmail.com>
2021-03-30 08:27:03 +0300

    video 57

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,17 +1,17 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
         <img
-            src="https://i.pravatar.cc/40"
+            src="{{$tweet->user->avatar}}"
             alt=""
             class="rounded-full mr-4"
         >
     </div>
 
     <div>
-        <h5 class="font-bold mb-4">Jogn Doe</h5>
+        <h5 class="font-bold mb-4">{{$tweet->user->name}}</h5>
 
         <p class="text-sm">
-            Presumably, if you're watching this series, you've already made the decision.
+            {{$tweet->body}}
         </p>
     </div>
 </div>
